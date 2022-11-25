b3da085b56bec4d0a572ec3c77713f5b1c177991
fengxiaoxiao <13156050650@163.com>
2022-11-25 12:36:01 +0800

    修改 镜像版本；数据库连接删除不用ssl；本地编译 fusion，application，gateway；biz 修改 protoc 版本；

--- primihub-webconsole/vue.config.js
+++ primihub-webconsole/vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
     },
     proxy: {
       '/dev-api': {
-        target: '',
+        target: 'http:localhost:8080',
         ws: true,
         changeOrigin: true,
         pathRewrite: {
